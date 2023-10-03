3a49e59778a06a2113f9457ab3163a33ff726867
Michael Litherland <michael.litherland@gmail.com>
2023-10-03 01:34:07 +0000

    Made subscriptions many to one with users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,45 +5,43 @@ class HomeController < ApplicationController
 
   def index
     @pub_key ||= Rails.configuration.vapid_public_key
-    @priv_key ||= Rails.configuration.vapid_private_key
-    sub = current_user.subscription
-    @push_info = {
-      enabled: sub.nil? ? false : true,
-      endpoint: sub.nil? ? nil : sub[:endpoint],
-      p256dh: sub.nil? ? nil : sub[:p256dh],
-      auth: sub.nil? ? nil : sub[:auth]
-    }
+    @subs_info = current_user.subscriptions
   end
 
   def enable
-    puts('subscribing')
     sub = Subscription.new
     sub[:user_id] = current_user.id
     sub[:endpoint] = params[:endpoint]
     sub[:p256dh] = params[:p256dh]
     sub[:auth] = params[:auth]
+    sub[:user_agent] = params[:user_agent]
+    sub[:enabled] = true
     sub.save
     render json: {}
   end
 
   def disable
-    sub = Subscription.where(user_id: current_user.id)
-    sub.destroy_all
+    endpoint = params[:endpoint]
+    current_user.subscriptions.each do |sub|
+      current_user.subscriptions.delete(sub) if sub.endpoint == endpoint
+    end
     render json: {}
   end
 
   def push_to_me
-    sub = current_user.subscription
-    if sub.nil?
+    subs = current_user.subscriptions
+    if subs.empty?
       redirect_to '/', alert: 'You do not have a subscription'
     else
-      send_sub(params[:message], sub)
-      redirect_to '/', notice: 'Attempted to send message'
+      subs.each do |sub|
+        send_sub(params[:message], sub) if sub.enabled
+      end
+      redirect_to '/', notice: 'Attempted to send messages'
     end
   end
 
   def push_to_all
-    subs = Subscription.all
+    subs = Subscription.where(enabled: true)
     subs.each { |sub| send_sub(params[:message], sub) }
     redirect_to '/', notice: "Attempted to send #{subs.count} messages"
   end
@@ -51,14 +49,13 @@ class HomeController < ApplicationController
   private
 
   def send_sub(message, sub)
-    puts('sending now')
     result = WebPush.payload_send(
       message: message,
       endpoint: sub[:endpoint],
       p256dh: sub[:p256dh],
       auth: sub[:auth],
       vapid: {
-        subject: "WebNote #{Time.now.to_i}",
+        subject: "mailto: michael.litherland@gmail.com",
         public_key: Rails.configuration.vapid_public_key,
         private_key: Rails.configuration.vapid_private_key
       },
@@ -66,11 +63,5 @@ class HomeController < ApplicationController
       open_timeout: 5,
       read_timeout: 5
     )
-    puts("response: #{result.code}")
-    puts('headers:')
-    result.header.each_header { |k, v| puts("- #{k} = #{v}") }
-    puts("body: #{result.body}")
-    puts('after sending')
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :subscription
+  has_many :subscriptions
 end
